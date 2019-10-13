51d188d33aad5290a539c52e7763e3617508a025
Yogesh Kannan <yogeshkannan@yogeshs-air.attlocal.net>
2019-10-13 11:09:56 -0700

    testing

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,27 @@
-#build docker images
-docker build -t ykannan108/multi-client:latest -t ykannan108/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t ykannan108/multi-server:latest -t ykannan108/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t ykannan108/multi-worker:latest -t ykannan108/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+# #build docker images
+# docker build -t ykannan108/multi-client:latest -t ykannan108/multi-client:$SHA -f ./client/Dockerfile ./client
+# docker build -t ykannan108/multi-server:latest -t ykannan108/multi-server:$SHA -f ./server/Dockerfile ./server
+# docker build -t ykannan108/multi-worker:latest -t ykannan108/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-#push docker latestimages
-docker push ykannan108/multi-client:latest
-docker push ykannan108/multi-server:latest
-docker push ykannan108/multi-worker:latest
+# #push docker latestimages
+# docker push ykannan108/multi-client:latest
+# docker push ykannan108/multi-server:latest
+# docker push ykannan108/multi-worker:latest
 
-#push images with tags
-docker push ykannan108/multi-client:$SHA
-docker push ykannan108/multi-server:$SHA
-docker push ykannan108/multi-worker:$SHA
+# #push images with tags
+# docker push ykannan108/multi-client:$SHA
+# docker push ykannan108/multi-server:$SHA
+# docker push ykannan108/multi-worker:$SHA
 
 #apply 
 kubectl apply -f k8s/application
-kubectl apply -f k8s/monitoring/prometheus
-kubectl apply -f k8s/monitoring/grafana
+kubectl apply -f k8s/monitoring/prometheus/
+kubectl apply -f k8s/monitoring/grafana/
 
-#set latest images on a deployment
-kubectl set image deployments/server-deployment server=ykannan108/multi-server:$SHA
-kubectl set image deployments/client-deployment server=ykannan108/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=ykannan108/multi-worker:$SHA
+# #set latest images on a deployment
+# kubectl set image deployments/server-deployment server=ykannan108/multi-server:$SHA
+# kubectl set image deployments/client-deployment server=ykannan108/multi-client:$SHA
+# kubectl set image deployments/worker-deployment server=ykannan108/multi-worker:$SHA
 
 #ignore the unstaged decrypted file
 git status -uno
